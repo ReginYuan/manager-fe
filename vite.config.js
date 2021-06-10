7c704b89dbbfcc80902987cd8ad83658238ade64
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2021-06-10 22:27:48 +0800

    5-6 面包屑实现

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
+  // vite可别名配置,解决./../问题,类似于vue里面的@
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   server: {
     host: 'localhost',//配置主机名
     port: 8080,//配置端口号
