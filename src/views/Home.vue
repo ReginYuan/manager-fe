3586d2a76aa9907a129abcd1ad61038bf74820fe
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2021-06-10 14:48:01 +0800

    5-2 首页菜单功能实现

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,69 @@
 <template>
   <div class="home">
     <div class="basic-layout">
-      <div class="nav-side"></div>
-      <div class="content-right">
+      <div :class="['nav-side', isCollapse ? 'fold' : 'unfold']">
+        <!-- 系统logo -->
+        <div class="logo">
+          <img src="./../assets/logo1.png" alt="" />
+          <span>Manager</span>
+        </div>
+        <!-- 导航菜单 -->
+        <el-menu
+          default-active="2"
+          background-color="#001529"
+          text-color="#fff"
+          router
+          :collapse="isCollapse"
+          class="nav-menu"
+        >
+          <!--el-submenu父菜单  -->
+          <el-submenu index="1">
+            <template #title>
+              <i class="el-icon-setting"></i>
+              <span>系统管理</span>
+            </template>
+            <!--el-menu-item子菜单  -->
+            <el-menu-item index="1-1">用户管理 </el-menu-item>
+            <el-menu-item index="1-2"> 菜单管理 </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template #title>
+              <i class="el-icon-setting"></i>
+              <span>审批管理</span>
+            </template>
+            <!--el-menu-item子菜单  -->
+            <el-menu-item index="2-1">休假申请</el-menu-item>
+            <el-menu-item index="2-2">待我审批</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </div>
+      <div :class="['content-right', isCollapse ? 'fold' : 'unfold']">
         <div class="nav-top">
-          <div class="bread">面包屑</div>
-          <div class="user-info">用户</div>
+          <div class="nav-left">
+            <div class="menu-fold" @click="toggle">
+              <i class="el-icon-s-fold"></i>
+            </div>
+            <div class="bread">面包屑</div>
+          </div>
+          <div class="user-info">
+            <el-badge :is-dot="true" class="notice" type="danger">
+              <i class="el-icon-bell"></i>
+            </el-badge>
+            <el-dropdown @command="handleLogout" :hide-on-click="false">
+              <span class="user-link">
+                {{ userInfo.userName }}
+                <i class="el-icon--right"></i>
+              </span>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item command="email">{{
+                    userInfo.userEmail
+                  }}</el-dropdown-item>
+                  <el-dropdown-item command="logout">退出</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </div>
         </div>
         <div class="wrapper">
           <div class="main-page">
@@ -18,7 +76,31 @@
 </template>
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data () {
+    return {
+      isCollapse: false,
+      userInfo: {
+        userName: 'ReginYuan',
+        userEmail: "ReginYuan@admin.com"
+      }
+    }
+  },
+  methods: {
+    // 退出功能
+    handleLogout (key) {
+      if (key == 'email') return;
+      // 设置localStorage数据为空
+      this.$store.commit('saveUserInfo', '');
+      // 清空当前数据
+      this.userInfo = null;
+      this.$router.push('/login')
+    },
+    // 控制左侧菜单隐藏
+    toggle () {
+      this.isCollapse = !this.isCollapse
+    }
+  }
 }
 </script>
 
@@ -35,10 +117,42 @@ export default {
     color: #fff;
     overflow-y: auto;
     transition: width 0.5s;
+
+    .logo {
+      display: flex;
+      align-items: center;
+      font-size: 18px;
+      height: 50px;
+      img {
+        margin: 0 16px;
+        width: 32px;
+        height: 32px;
+      }
+    }
+    .nav-menu {
+      height: calc(100vh-50px);
+      border-right: none;
+    }
+    // 合并左侧菜单
+    &.fold {
+      width: 64px;
+    }
+    // 展开左侧菜单
+    &.unfold {
+      width: 200px;
+    }
   }
   /** 右侧页面内容样式**/
   .content-right {
     margin-left: 200px;
+    // 合并左侧菜单
+    &.fold {
+      margin-left: 64px;
+    }
+    // 展开左侧菜单
+    &.unfold {
+      margin-left: 200px;
+    }
     /** 右侧导航内容样式**/
     .nav-top {
       height: 50px;
@@ -47,6 +161,24 @@ export default {
       justify-content: space-between;
       border-bottom: 1px solid #ddd;
       padding: 0 20px;
+      .nav-left {
+        display: flex;
+        align-items: center;
+        .menu-fold {
+          margin-right: 15px;
+          font-size: 18px;
+        }
+      }
+      .user-info {
+        .notice {
+          line-height: 30px;
+          margin-right: 15px;
+        }
+        .user-link {
+          cursor: pointer;
+          color: #409fff;
+        }
+      }
     }
     /**内容样式**/
     .wrapper {
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -64,7 +64,7 @@ export default {
   }
 }
 </script>
-<style  lang="scss" scoped>
+<style  lang="scss" >
 .login-wrapper {
   display: flex;
   justify-content: center;
